4bdc9f5f9465e42e4608ba8b1ec955530b6d1beb
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-03-18 09:57:02 -0700

    Cleared out code to practice with

--- src/Build.java
+++ src/Build.java
@@ -15,28 +15,7 @@ public class Build {
    */
   public static void printShortWords(Vertex<String> vertex, int k) {
 
-    Set<String> visited = new HashSet<>();
-    pSWHelper(vertex, k, visited);
-  }
-
-  private static void pSWHelper(Vertex<String> vertex, int k, Set<String> visited) {
-
-    if (vertex == null) {
-      return;
-    }
-
-    if (visited.contains(vertex.data)) {
-      return;
-    }
-
-    if (vertex.data.length() < k) {
-      System.out.println(vertex.data);
-      visited.add(vertex.data);
-    }
-
-    for (var neighbor : vertex.neighbors) {
-      pSWHelper(neighbor, k, visited);
-    }
+   
   }
 
   /**
@@ -47,35 +26,11 @@ public class Build {
    */
   public static String longestWord(Vertex<String> vertex) {
 
-    Set<String> visited = new HashSet<>();
-
-    return lWHelper(vertex, visited, "");
+    
+    return null;
   }
 
-  private static String lWHelper(Vertex<String> vertex, Set<String> visited, String longestWord) {
-
-    if (vertex == null || visited.contains(vertex.data)) {
-      return longestWord;
-    }
-
-    visited.add(vertex.data);
-
-    if (vertex.data.length() > longestWord.length()) {
-      longestWord = vertex.data;
-    }
-
-    String currentLongest = vertex.data;
-
-    for (var neighbor : vertex.neighbors) {
-      currentLongest = lWHelper(neighbor, visited, longestWord);
-      
-      if (currentLongest.length() > longestWord.length()) {
-        longestWord = currentLongest;
-      }
-    }
-
-    return longestWord;
-  }
+  
 
   /**
    * Prints the values of all vertices that are reachable from the given vertex and 
@@ -86,26 +41,10 @@ public class Build {
    */
   public static <T> void printSelfLoopers(Vertex<T> vertex) {
 
-    Set<T> visited = new HashSet<>();
-    pSLHelper(vertex, visited);
+   
   }
 
-  private static <T> void pSLHelper(Vertex<T> vertex, Set<T> visited) {
-    if (vertex == null) {
-      return;
-    }
-
-    if (visited.contains(vertex.data)) {
-      System.out.println(vertex.data);
-      return;
-    }
-
-    visited.add(vertex.data);
-
-    for (var neighbor : vertex.neighbors) {
-      pSLHelper(neighbor, visited);
-    }
-  }
+  
 
   /**
    * Determines whether it is possible to reach the destination airport through a series of flights
@@ -116,30 +55,10 @@ public class Build {
    * @return true if the destination is reachable from the start, false otherwise
    */
   public static boolean canReach(Airport start, Airport destination) {
-    Set<Airport> visited = new HashSet<>();
-    return cRHelper(start, destination, visited);
-  }
-
-  public static boolean cRHelper(Airport start, Airport destination, Set<Airport> visited) {
-
-   if (start == null || visited.contains(start)) {
-      return false;
-    }
-
-    if (start == destination) {
-      return true;
-    }
-
-    visited.add(start);
-
-    for (Airport flight : start.getOutboundFlights()) {
-      if (cRHelper(flight, destination, visited)) {
-        return true;
-      }
-    }
-
     return false;
   }
+
+  
 
   /**
    * Returns the set of all values in the graph that cannot be reached from the given starting value.
@@ -151,23 +70,8 @@ public class Build {
    * @return a set of values that cannot be reached from the starting value
    */
   public static <T> Set<T> unreachable(Map<T, List<T>> graph, T starting) {
-    Set<T> visited = new HashSet<>();
-    unreachableHelper(graph, starting, visited);
+    
 
-    Set<T> cantVisit = new HashSet<>(graph.keySet());
-    cantVisit.removeAll(visited);
-
-    return cantVisit;
-  }
-
-  public static <T> void unreachableHelper(Map<T, List<T>> graph, T starting, Set<T> visited) {
-
-    if (!graph.containsKey(starting) || graph == null || !visited.add(starting)) {
-      return;
-    }
-
-    for (var neighbor : graph.get(starting)) {
-      unreachableHelper(graph, neighbor, visited);
-    }
+    return null;
   }
 }
